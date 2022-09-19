71f01ef307dd4d264431563e2392a839ad74ce17
Hansaka Anuhas <113702754+SLHansakaAnuhas@users.noreply.github.com>
2022-09-19 08:05:59 +0530

    SLFiltersBot

--- script.py
+++ script.py
@@ -5,15 +5,15 @@ I'm An Advanced Filters Bot 😎"""
 
     HELP_MSG = """Add Me As Admin In Your Group And Start Filtering
 
-<b>Filter Commands:</b>
+<b>Filters Commands:</b>
 /filter - Add Filter
 /del - Delete Filter
 /delall - Delete All filters
 /filters - List All Filters
 
-<b>Connection Commands:</b>
+<b>Connections Commands:</b>
 /connect - Connect PM
 /connections - List All Connections"""
 
     ABOUT_MSG = """My Name: SL Filters Bot
-Creater: <a href=https://t.me/Hansaka_Anuhas>Hansaka Anuhas</a> 🇱🇰"""
+Creator: <a href=https://t.me/Hansaka_Anuhas>Hansaka Anuhas</a> 🇱🇰"""
