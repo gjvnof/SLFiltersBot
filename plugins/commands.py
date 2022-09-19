5c11b8b53b2a756987a2e1c515cf7c92574c545a
Hansaka Anuhas <113702754+SLHansakaAnuhas@users.noreply.github.com>
2022-09-19 08:24:23 +0530

    SLFiltersBot

--- plugins/commands.py
+++ plugins/commands.py
@@ -212,13 +212,11 @@ async def bot_status(client,message):
 @trojanz.on_message(filters.command('start') & filters.private)
 async def start(client, message):
     await message.reply_text(
-        text=Script.START_MSG.format(message.from_user.mention,temp.U_NAME),
+        text=Script.START_MSG.format(message.from_user.mention),
         disable_web_page_preview=True,
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton('➕ Add Me As Admin In Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
-                    ],[
                     InlineKeyboardButton("❓ Help", callback_data="help_data")
                 ]
             ]
